6f51748561d94210765d0fbbe86c5f4dc2646768
Alberto Basalo <albertobasalo@agorabinaria.com>
2018-04-04 13:58:07 +0200

    getMonthBalance initial

--- src/app/routes/month/state/month-balance/month-balance.effects.ts
+++ src/app/routes/month/state/month-balance/month-balance.effects.ts
@@ -1,9 +1,24 @@
-import { Injectable } from '@angular/core';
-import { Actions, Effect } from '@ngrx/effects';
-
+import { Injectable } from "@angular/core";
+import { Actions, Effect, ofType } from "@ngrx/effects";
+import { Observable } from "rxjs/Observable";
+import { Action } from "@ngrx/store";
+import {
+  GetMonthBalance,
+  MonthBalanceActionTypes
+} from "@routes/month/state/month-balance/month-balance.actions";
+import { switchMap } from "rxjs/operators";
 
 @Injectable()
 export class MonthBalanceEffects {
-
+  private onGetMonthBalance$ = (action: GetMonthBalance) => {
+    console.log("onGetMonthBalance$");
+    return null;
+  };
   constructor(private actions$: Actions) {}
+
+  @Effect()
+  public getMonthBalance$: Observable<Action> = this.actions$.pipe(
+    ofType<GetMonthBalance>(MonthBalanceActionTypes.GetMonthBalance),
+    switchMap(this.onGetMonthBalance$)
+  );
 }
