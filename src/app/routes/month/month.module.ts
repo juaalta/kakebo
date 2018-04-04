af757b415c96636b6887e1db9abeb27b31e3ee66
Alberto Basalo <albertobasalo@agorabinaria.com>
2018-04-04 13:50:02 +0200

    ng g ef MonthBalance --spec false -m routes/month/month.module.ts

--- src/app/routes/month/month.module.ts
+++ src/app/routes/month/month.module.ts
@@ -9,13 +9,16 @@ import { StoreService } from "@routes/month/store.service";
 import { Reducers } from "@routes/month/reducers.service";
 import { StoreModule } from "@ngrx/store";
 import { reducers, metaReducers } from "@routes/month/state";
+import { EffectsModule } from '@ngrx/effects';
+import { MonthBalanceEffects } from '../../month-balance.effects';
 
 @NgModule({
   imports: [
     CommonModule,
     ControlRoutingModule,
     ComponentsModule,
-    StoreModule.forFeature("month", reducers, { metaReducers })
+    StoreModule.forFeature("month", reducers, { metaReducers }),
+    EffectsModule.forFeature([MonthBalanceEffects])
   ],
   declarations: [MonthComponent],
   providers: [ApiService, StoreService, Reducers]
