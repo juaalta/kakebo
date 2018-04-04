6cebc3c6ac0e7c8f671ea4eab1f13000d565be44
Alberto Basalo <albertobasalo@agorabinaria.com>
2018-04-04 13:04:02 +0200

    ng g st Month --statePath state --module routes/month/month.module.ts --stateInterface Month

--- src/app/routes/month/month.module.ts
+++ src/app/routes/month/month.module.ts
@@ -7,9 +7,11 @@ import { ComponentsModule } from "@tools/components/components.module";
 import { ApiService } from "@routes/month/api.service";
 import { StoreService } from "@routes/month/store.service";
 import { Reducers } from "@routes/month/reducers.service";
+import { StoreModule } from '@ngrx/store';
+import * as fromMonth from '../../state';
 
 @NgModule({
-  imports: [CommonModule, ControlRoutingModule, ComponentsModule],
+  imports: [CommonModule, ControlRoutingModule, ComponentsModule, StoreModule.forFeature('month', fromMonth.reducers, { metaReducers: fromMonth.metaReducers })],
   declarations: [MonthComponent],
   providers: [ApiService, StoreService, Reducers]
 })
