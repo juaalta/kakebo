7a4301eefe845371fd6feabc46aa00ac9f3f8d27
Alberto Basalo <albertobasalo@hotmail.com>
2018-04-05 12:34:56 +0200

    journal entry reducers

---
+++ src/app/routes/month/state/journal-entry/journal-entry.reducer.ts
@@ -0,0 +1,32 @@
+import { Action } from "@ngrx/store";
+import { JournalEntryActions, JournalEntryActionTypes } from "@routes/month/state/journal-entry/journal-entry.actions";
+import { JournalEntry, journalEntriesInitialState } from "@routes/month/state/journal-entry/models/journal_entry.model";
+
+export function journalEntryReducer(
+  state = journalEntriesInitialState,
+  action: JournalEntryActions
+): JournalEntry[] {
+  console.log(action.type, action.payload);
+  switch (action.type) {
+    case JournalEntryActionTypes.GetJournalEntries:
+      return state;
+    case JournalEntryActionTypes.GetJournalEntriesCompleted:
+      return action.payload;
+    case JournalEntryActionTypes.GetJournalEntriesFailed:
+      return action.payload;
+    case JournalEntryActionTypes.PostJournalEntry:
+      return state;
+    case JournalEntryActionTypes.PostJournalEntryCompleted:
+      return [...state, action.payload];
+    case JournalEntryActionTypes.PostJournalEntryFailed:
+      return state;
+    case JournalEntryActionTypes.DeleteJournalEntry:
+      return state;
+    case JournalEntryActionTypes.DeleteJournalEntryCompleted:
+      return state.filter(j=>j._id!==action.payload._id);
+    case JournalEntryActionTypes.DeleteJournalEntryFailed:
+      return state;
+    default:
+      return state;
+  }
+}
