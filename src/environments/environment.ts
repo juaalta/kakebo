acd320a24501b84f5702f079500ac1a1bf5f7159
Alberto Basalo <albertobasalo@hotmail.com>
2018-04-08 11:52:57 +0200

    api heroku

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: "http://localhost:3030/api/"
+  //apiUrl: "http://localhost:3030/api/"
+  apiUrl: "http://api-base.escuelabinaria.com/api/"
 };
 
 /*
