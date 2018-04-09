defd0621a3e9f997916d09f3c37d57f9ca6858f4
Alberto Basalo <albertobasalo@hotmail.com>
2018-04-09 18:02:39 +0200

    api service moved

--- src/app/routes/month/month.module.ts
+++ src/app/routes/month/month.module.ts
@@ -4,8 +4,8 @@ import { CommonModule } from "@angular/common";
 import { ControlRoutingModule } from "./month-routing.module";
 import { MonthComponent } from "./month.component";
 import { ComponentsModule } from "@tools/components/components.module";
-import { ApiService } from "@routes/month/api.service";
 import { MonthStore } from "@routes/month/state/month-store.state";
+import { ApiService } from "@routes/month/state/api.service";
 
 @NgModule({
   imports: [CommonModule, ControlRoutingModule, ComponentsModule],
