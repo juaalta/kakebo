da958235d1bc7e507fdae984bec35cbe6d2e49ea
Alberto Basalo <albertobasalo@hotmail.com>
2018-04-23 17:03:44 +0200

    base app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'kab-root',
-  template: `
-    <header>
-      <h1>Kakebo</h1>
-      <blockquote>
-        <p><em>The japanese art of saving money</em></p>
-      </blockquote>
-    </header>  
-    <router-outlet></router-outlet>
-    
-  `,
-  styles: []
+	selector: "kab-root",
+	templateUrl: "./app.component.html",
+	styles: []
 })
 export class AppComponent {
-  title = 'kab';
+	title = "kab";
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,11 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-
-import { AppRoutingModule } from "./app-routing.module";
-
-import { ServiceWorkerModule } from "@angular/service-worker";
 import { AppComponent } from "./app.component";
 
-import { environment } from "../environments/environment";
-import { GlobalModule } from "@tools/global/global.module";
-
 @NgModule({
-  declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ServiceWorkerModule.register("/ngsw-worker.js", {
-      enabled: environment.production
-    }),
-    GlobalModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent],
+	imports: [BrowserModule],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule {}
