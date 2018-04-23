6662218f3e3240c211caa9eaf2c8b1af0eb0817e
Alberto Basalo <albertobasalo@hotmail.com>
2018-04-23 18:34:30 +0200

    spa layout

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { HomeComponent } from "./home/home.component";
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
+const routes: Routes = [
+	{
+		path: "",
+		component: HomeComponent
+	}
+];
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+	imports: [RouterModule.forChild(routes)],
+	exports: [RouterModule]
 })
-export class HomeRoutingModule { }
+export class HomeRoutingModule {}
