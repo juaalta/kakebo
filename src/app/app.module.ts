e85e1d5bd4263b8dba033f61dad2ae4de93df22f
Alberto Basalo <albertobasalo@hotmail.com>
2018-04-23 18:25:11 +0200

    spa loop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { BrowserModule } from "@angular/platform-browser";
 import { CoreModule } from "./core/core.module";
-import { HomeModule } from "./home/home.module";
+import { NgModule } from "@angular/core";
 
 @NgModule({
 	declarations: [AppComponent],
-	imports: [BrowserModule, CoreModule, HomeModule],
+	imports: [AppRoutingModule, BrowserModule, CoreModule],
 	providers: [],
 	bootstrap: [AppComponent]
 })
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,12 +1,10 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HomeComponent } from './home/home.component';
+import { CommonModule } from "@angular/common";
+import { HomeComponent } from "./home/home.component";
+import { HomeRoutingModule } from "./home-routing.module";
+import { NgModule } from "@angular/core";
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: [HomeComponent],
-  exports: [HomeComponent]
+	imports: [CommonModule, HomeRoutingModule],
+	declarations: [HomeComponent]
 })
-export class HomeModule { }
+export class HomeModule {}
