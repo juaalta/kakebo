988737148de7f07b1ae31884767b2d31a84c69eb
Alberto Basalo <albertobasalo@hotmail.com>
2018-04-23 17:04:02 +0200

    delete old

--- src/app/routes/home/home.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { ComponentsModule } from "@tools/components/components.module";
-import { HomeRoutingModule } from "./home-routing.module";
-@NgModule({
-  imports: [CommonModule, ComponentsModule, HomeRoutingModule],
-  declarations: []
-})
-export class HomeModule {}
