a3d15f40c4195247fcef48d7bb68ace42fd67584
Alberto Basalo <albertobasalo@hotmail.com>
2018-04-26 13:48:52 +0200

    nested routes

--- src/app/balance/balance-routing.module.ts
+++ src/app/balance/balance-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BalanceComponent } from './balance.component';
+import { ForecastsComponent } from './forecasts/forecasts.component';
+import { ExpensesComponent } from './expenses/expenses.component';
 
 const routes: Routes = [
   {
     path: '',
     component: BalanceComponent
+  },
+  {
+    path: 'forecasts',
+    component: ForecastsComponent
+  },
+  {
+    path: 'expenses',
+    component: ExpensesComponent
   }
 ];
 
--- src/app/balance/balance.module.ts
+++ src/app/balance/balance.module.ts
@@ -2,31 +2,31 @@ import { BalanceComponent } from './balance.component';
 import { BalanceRoutingModule } from './balance-routing.module';
 import { CommonModule } from '@angular/common';
 import { ExpensesComponent } from './expenses/expenses.component';
+import { ForecastsComponent } from './forecasts/forecasts.component';
 import { FormsModule } from '@angular/forms';
 import { JournalEntryService } from './state/journal-entry.service';
 import { ListExpensesComponent } from './expenses/list-expenses/list-expenses.component';
+import { ListForecastsComponent } from './forecasts/list-forecasts/list-forecasts.component';
 import { NewExpenseComponent } from './expenses/new-expense/new-expense.component';
+import { NewForecastComponent } from './forecasts/new-forecast/new-forecast.component';
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
-import { ForecastsComponent } from './forecasts/forecasts.component';
-import { NewForecastComponent } from './forecasts/new-forecast/new-forecast.component';
-import { ListForecastsComponent } from './forecasts/list-forecasts/list-forecasts.component';
 
 @NgModule({
   imports: [
-    CommonModule,
     BalanceRoutingModule,
+    CommonModule,
     FormsModule,
     SharedModule
   ],
   declarations: [
     BalanceComponent,
     ExpensesComponent,
-    NewExpenseComponent,
-    ListExpensesComponent,
     ForecastsComponent,
-    NewForecastComponent,
-    ListForecastsComponent
+    ListExpensesComponent,
+    ListForecastsComponent,
+    NewExpenseComponent,
+    NewForecastComponent
   ],
   providers: [JournalEntryService]
 })
