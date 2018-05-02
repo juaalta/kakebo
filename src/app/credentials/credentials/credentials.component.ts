ab4c626a32937dcfc9ea94d2be1ea4778ef9530b
Alberto Basalo <albertobasalo@hotmail.com>
2018-05-02 10:25:54 +0200

    credentials reactive

--- src/app/credentials/credentials/credentials.component.ts
+++ src/app/credentials/credentials/credentials.component.ts
@@ -5,6 +5,11 @@ import { Credential } from '../store/models/credential.model';
 import { CredentialsPageData } from '../store/models/credentials-page-data.model';
 import { CredentialsService } from '../store/credentials.service';
 import { GlobalStoreService } from '../../core/store/global-store.service';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators
+} from '@angular/forms';
 
 @Component({
   selector: 'kab-credentials',
@@ -13,11 +18,12 @@ import { GlobalStoreService } from '../../core/store/global-store.service';
 })
 export class CredentialsComponent implements OnInit {
   public pageData: CredentialsPageData;
-  public credential: Credential;
+  public form: FormGroup;
   public mustShowErrors = this.formsService.mustShowErrors;
   constructor(
     private activatedRoute: ActivatedRoute,
     private credentialsService: CredentialsService,
+    private formBuilder: FormBuilder,
     private formsService: FormsService,
     private globalStore: GlobalStoreService,
     private router: Router
@@ -29,16 +35,30 @@ export class CredentialsComponent implements OnInit {
   private obtainPageDataFromRoute() {
     this.pageData = this.activatedRoute.snapshot
       .data as CredentialsPageData;
-    this.credential = this.pageData.credential;
+    const credential = this.pageData.credential;
+    this.form = this.formBuilder.group({
+      email: [
+        credential.email,
+        [Validators.required, Validators.email]
+      ],
+      password: [
+        credential.password,
+        [
+          Validators.required,
+          Validators.minLength(4)
+          // ValidatePassword
+        ]
+      ]
+    });
   }
 
-  public onSubmitCredential = () => {
+  public onSubmitCredential = (credential: Credential) => {
     this.globalStore.dispatchUserMessage(
       'Validating Credentials...'
     );
     const endPoint = this.pageData.endPoint;
     this.credentialsService
-      .sendCredential(this.credential, endPoint)
+      .sendCredential(credential, endPoint)
       .subscribe(
         this.acceptedCredentials,
         this.invalidCredentials
