354bd21068b89f24a8e99ea04ab661d6e19f634d
Alberto Basalo <albertobasalo@hotmail.com>
2018-05-21 08:59:55 +0200

    app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,13 @@ import { GlobalStoreService } from './core/store/global-store.service';
 })
 export class AppComponent implements OnInit {
   public userMessage$: Observable<string>;
+  public userMessage: string;
   public userIsAnonymous$: Observable<boolean>;
   constructor(private globalStore: GlobalStoreService) {}
 
   ngOnInit(): void {
+    this.globalStore.selectUserMessage$().subscribe(res => this.userMessage = res);
+    
     this.userMessage$ = this.globalStore.selectUserMessage$();
     this.userIsAnonymous$ = this.globalStore.selectUserIsAnonymous$();
   }
